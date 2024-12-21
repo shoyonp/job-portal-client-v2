6b446444a3697f224f59a1ea18513f2e01420933
shoyonp <shoyonkumar143@gmail.com>
2024-12-21 14:20:16 +0600

    token generate and remove done

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -41,15 +41,29 @@ const AuthProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       console.log("state captured", currentUser?.email);
-      if(currentUser?.email){
-        const user = {email: currentUser.email}
+      if (currentUser?.email) {
+        const user = { email: currentUser.email };
 
-        axios.post('http://localhost:5000/jwt',user) 
-        .then(res => console.log(res.data))
+        axios
+          .post("http://localhost:5000/jwt", user, { withCredentials: true })
+          .then((res) => {
+            console.log("login token", res.data);
+            setLoading(false);
+          });
+      } else {
+        axios
+          .post(
+            "http://localhost:5000/logout",
+            {},
+            {
+              withCredentials: true,
+            }
+          )
+          .then((res) => {
+            console.log("logout", res.data);
+            setLoading(false);
+          });
       }
-  
-        // put it in the right place
-      setLoading(false);
     });
 
     return () => {
